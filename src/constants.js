4a28aea99e27a24501e30f86e14e7af104413b03
Khizar Ali <s.khizarali03@gmail.com>
2024-07-30 22:15:13 +0500

    all setup configured

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "FYP-CDP"
---
+++ src/index.js
@@ -0,0 +1,19 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/db.js";
+import {app} from './app.js'
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
