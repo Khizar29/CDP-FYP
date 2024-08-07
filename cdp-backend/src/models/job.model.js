88f898b792fe8e4bd6503d9886ce45049ee9ecc2
Khizar Ali <s.khizarali03@gmail.com>
2024-08-08 00:12:02 +0500

    Job schema created

---
+++ cdp-backend/src/models/job.model.js
@@ -0,0 +1,50 @@
+// src/models/jobs.model.js
+import mongoose, { Schema } from "mongoose";
+
+const jobSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    job_type: {
+      type: String,
+      enum: ["Onsite", "Remote", "Hybrid", "Internship"],
+      required: true,
+    },
+    no_of_openings: {
+      type: Number,
+      required: true,
+    },
+    qualification_req: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    job_description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    responsibilities: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    posted_on: {
+      type: Date,
+      default: Date.now,
+    },
+    updated_on: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true, // Automatically manage createdAt and updatedAt fields
+  }
+);
+
+const Job = mongoose.model("Job", jobSchema);
+
+export default Job;
