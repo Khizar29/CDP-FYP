2e4170660b270910da3f656919a911fd140805f6
Khizar Ali <s.khizarali03@gmail.com>
2024-08-08 07:14:54 +0500

    Styling for hero-section

--- cdp-frontend/src/components/Home.js
+++ cdp-frontend/src/components/Home.js
@@ -8,7 +8,7 @@ import About from './About';
 const Home = ({ aboutRef }) => {
   return (
     <div>
-      <section id="home" className="relative h-screen overflow-hidden">
+      <section id="home" className="relative h-screen rounded-3xl m-10 overflow-hidden">
         <div
           className="absolute inset-0 bg-cover bg-center z-0"
           style={{
@@ -17,8 +17,8 @@ const Home = ({ aboutRef }) => {
         ></div>
         <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
         <div className="relative flex flex-col items-center justify-center h-full text-white text-center z-20">
-          <h1 className="text-4xl md:text-6xl font-bold">Welcome to FAST ALUMNI & Career Portal</h1>
-          <p className="text-xl md:text-2xl mt-4">Connecting CS alumni across the globe</p>
+          <h1 className="text-4xl md:text-6xl font-bold">Welcome <span className='text-white'>to </span> <br/>FAST <span className='text-yellow-500'>Career </span>Developement <span className='text-sky-600'>Portal</span> </h1>
+          <p className="text-xl md:text-2xl mt-4">Your Gateway to a Successful Career Journey</p>
           <div className="mt-8 space-x-4">
             <ScrollLink to="about" smooth={true} duration={500} className="bg-blue-900 text-white py-2 px-4 rounded-full hover:bg-blue-600 transition duration-300 cursor-pointer">
               Find Out More
