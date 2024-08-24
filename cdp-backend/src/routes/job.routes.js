2dcbbdf65fe78e3e678e8d9ae5d539df7564e5d0
Khizar Ali <s.khizarali03@gmail.com>
2024-08-24 14:22:57 +0500

    job-count

--- cdp-backend/src/routes/job.routes.js
+++ cdp-backend/src/routes/job.routes.js
@@ -1,4 +1,3 @@
-// src/routes/job.routes.js
 import { Router } from 'express';
 import {
   createJob,
@@ -6,17 +5,21 @@ import {
   deleteJob,
   getAllJobs,
   getJobById,
+  getJobCount
 } from '../controllers/job.controller.js';
-import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { verifyJWT, verifyAdmin } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 // Public routes
+router.get('/count', getJobCount); 
 router.route('/').get(getAllJobs);
 router.route('/:jobId').get(getJobById);
 
 // Admin routes
 router.use(verifyJWT); // Ensure user is logged in
+router.use(verifyAdmin); // Ensure user is admin
+
 router.route('/').post(createJob);
 router.route('/:jobId').put(updateJob).delete(deleteJob);
 
