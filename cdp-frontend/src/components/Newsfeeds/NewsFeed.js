bf21bd9369a265d21992b8ef0e0749cc4eb68fcd
Khizar Ali <s.khizarali03@gmail.com>
2024-10-18 23:45:14 +0500

    fixes

--- cdp-frontend/src/components/Newsfeeds/NewsFeed.js
+++ cdp-frontend/src/components/Newsfeeds/NewsFeed.js
@@ -9,13 +9,17 @@ import ArticleIcon from '@mui/icons-material/Article';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 
 const NewsFeed = () => {
-  const [newsItems, setNewsItems] = useState([]);
+  const [newsItems, setNewsItems] = useState([]); // Initialize state as an empty array
   const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/newsfeeds?isPublic=true&limit=4`)
       .then(response => {
-        setNewsItems(response.data.data);
+        if (response.data && Array.isArray(response.data.data)) {
+          setNewsItems(response.data.data); // Set data only if it's an array
+        } else {
+          console.error("Unexpected data format", response.data); // Log if unexpected format
+        }
       })
       .catch(error => {
         console.error("There was an error fetching the news items!", error);
@@ -29,77 +33,84 @@ const NewsFeed = () => {
   const getIcon = (category) => {
     switch (category.toLowerCase()) {
       case 'event':
-        return <EventIcon/>
+        return <EventIcon />;
       case 'announcement':
-        return <AnnouncementIcon/>
+        return <AnnouncementIcon />;
       case 'news':
       default:
-        return <ArticleIcon/> 
+        return <ArticleIcon />;
     }
   };
 
   const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
   return (
-    <Box sx={{ width: '100%', padding: '2rem', backgroundColor: '#0b5394', borderRadius:'10px'}}>
+    <Box sx={{ width: '100%', padding: '2rem', backgroundColor: '#0b5394', borderRadius: '10px' }}>
       <Typography variant="h3" align="center" color="#fff" gutterBottom>
         Latest <span className='text-yellow-500 font-bold'>News </span>
         and <span className='text-green-600 font-bold'>Events</span>
       </Typography>
       <Grid container spacing={4}>
-        {newsItems.map((newsItem) => (
-          <Grid item key={newsItem._id} xs={12} sm={6} md={4} lg={3}>
-            <Card
-              sx={{
-                maxWidth: 345,
-                height: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                boxShadow: 3,
-                borderRadius: 3,
-                border: '3px solid #fa8306',
-                transition: 'transform 0.3s, box-shadow 0.3s',
-                '&:hover': { transform: 'scale(1.03)', boxShadow: 6 },
-                overflow: 'hidden',
-              }}
-              onClick={() => openNewsDetail(newsItem._id)}
-            >
-              <CardMedia
-                component="img"
-                height="180"
-                image={newsItem.image}
-                alt={newsItem.title}
-                sx={{ objectFit: 'cover' }}
-              />
-              <CardContent sx={{ flexGrow: 1, textAlign: 'center' }}>
-              <Box sx={{ mb: 1 }}>
-                  <Chip
-                    label={capitalize(newsItem.category)}
-                    icon={getIcon(newsItem.category)}
-                    sx={{
-                      backgroundColor: '#50acff',
-                      padding: 1,                      color: '#fff', // Text color inside the Chip
-                      fontWeight: 'bold',
-                      '.MuiChip-icon': {
-                        color: '#fff' // Icon color inside the Chip
-                      },
-                    }}
-                  />
-                </Box>  
-                <Typography variant="h6" component="div" color="primary" gutterBottom>
-                  {newsItem.title}
-                </Typography>
-                <Box display="flex" alignItems="center" justifyContent="center" mt={1}>
-                  <CalendarTodayIcon fontSize="small" sx={{ mr: 0.5, color: 'gray' }} />
-                  <Typography variant="body2" color="text.secondary">
-                    Posted on {new Date(newsItem.date).toLocaleDateString()}
+        {newsItems && Array.isArray(newsItems) && newsItems.length > 0 ? (
+          newsItems.map((newsItem) => (
+            <Grid item key={newsItem._id} xs={12} sm={6} md={4} lg={3}>
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                  boxShadow: 3,
+                  borderRadius: 3,
+                  border: '3px solid #fa8306',
+                  transition: 'transform 0.3s, box-shadow 0.3s',
+                  '&:hover': { transform: 'scale(1.03)', boxShadow: 6 },
+                  overflow: 'hidden',
+                }}
+                onClick={() => openNewsDetail(newsItem._id)}
+              >
+                <CardMedia
+                  component="img"
+                  height="180"
+                  image={newsItem.image}
+                  alt={newsItem.title}
+                  sx={{ objectFit: 'cover' }}
+                />
+                <CardContent sx={{ flexGrow: 1, textAlign: 'center' }}>
+                  <Box sx={{ mb: 1 }}>
+                    <Chip
+                      label={capitalize(newsItem.category)}
+                      icon={getIcon(newsItem.category)}
+                      sx={{
+                        backgroundColor: '#50acff',
+                        padding: 1,
+                        color: '#fff', // Text color inside the Chip
+                        fontWeight: 'bold',
+                        '.MuiChip-icon': {
+                          color: '#fff' // Icon color inside the Chip
+                        },
+                      }}
+                    />
+                  </Box>
+                  <Typography variant="h6" component="div" color="primary" gutterBottom>
+                    {newsItem.title}
                   </Typography>
-                </Box>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+                  <Box display="flex" alignItems="center" justifyContent="center" mt={1}>
+                    <CalendarTodayIcon fontSize="small" sx={{ mr: 0.5, color: 'gray' }} />
+                    <Typography variant="body2" color="text.secondary">
+                      Posted on {new Date(newsItem.date).toLocaleDateString()}
+                    </Typography>
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          <Typography variant="body1" color="#fff">
+            No news items available or still loading...
+          </Typography>
+        )}
       </Grid>
       <Box display="flex" justifyContent="center" mt={4}>
         <Button
