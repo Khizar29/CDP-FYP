ece8db891fa664f7626e00e6ca0e141bb86d0504
Khizar Ali <s.khizarali03@gmail.com>
2024-10-19 15:41:43 +0500

    fix v2

--- cdp-backend/src/app.js
+++ cdp-backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({
     //  origin: "http://192.168.100.3:3000",  
-    origin: "http://10.11.0.23:3000",  
+    origin: "http://10.11.67.67:3000",  
     // origin: process.env.CORS_ORIGIN || "http://localhost:3000",
     credentials: true
 }));
