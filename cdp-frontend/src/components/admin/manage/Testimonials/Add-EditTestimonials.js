b0ee3adffcbd7da1f7f54d79b40dedf512f84396
Khizar Ali <s.khizarali03@gmail.com>
2024-10-25 15:13:13 +0500

    Manage Testimonials Admin

---
+++ cdp-frontend/src/components/admin/manage/Testimonials/Add-EditTestimonials.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+const AddEditTestimonial = () => {
+    const location = useLocation();
+    const testimonial = location.state?.data;
+
+    const [formData, setFormData] = useState({
+        name: testimonial?.name || '',
+        message: testimonial?.message || '',
+        title: testimonial?.title || '',
+        isApproved: testimonial?.isApproved || false
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('FormData before submission: ', formData); // Check if formData contains correct isApproved value
+    
+        try {
+            const token = localStorage.getItem("accessToken");
+    
+            if (!token) {
+                throw new Error('No token found');
+            }
+    
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            };
+    
+            if (testimonial) {
+                await axios.put(`${process.env.REACT_APP_BACKEND_URL}/api/v1/testimonials/${testimonial._id}`, formData, config);
+            } else {
+                await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/v1/testimonials`, formData, config);
+            }
+    
+            navigate('/admin/testimonials');
+        } catch (error) {
+            console.error('Error saving testimonial:', error);
+            alert('Error saving testimonial: ' + error.message);
+        }
+    };
+    
+
+    return (
+        <div className="container mx-auto p-8">
+            <form onSubmit={handleSubmit} className="bg-white p-10 rounded-lg shadow-lg max-w-2xl mx-auto">
+                <h2 className="text-2xl font-bold mb-6 text-center text-blue-900">Add / Edit Testimonial</h2>
+
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                        Name
+                    </label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-900 focus:ring-2 focus:ring-blue-900 transition duration-200"
+                        required
+                    />
+                </div>
+
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+                        Message
+                    </label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-900 focus:ring-2 focus:ring-blue-900 transition duration-200"
+                        rows="6"
+                        required
+                    />
+                </div>
+
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">
+                        Title / Position
+                    </label>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        value={formData.title}
+                        onChange={handleChange}
+                        className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-900 focus:ring-2 focus:ring-blue-900 transition duration-200"
+                    />
+                </div>
+
+                <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="isApproved">
+                        Approved
+                    </label>
+                    <select
+                    id="isApproved"
+                    name="isApproved"
+                    value={formData.isApproved}
+                    onChange={(e) => setFormData({ ...formData, isApproved: e.target.value === 'true' })}
+                    className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-900 focus:ring-2 focus:ring-blue-900 transition duration-200"
+                    >
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </select>
+                </div>
+
+                <div className="flex items-center justify-between">
+                    <button
+                        type="submit"
+                        className="bg-blue-900 text-white py-2 px-6 rounded-lg font-bold hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50 transition duration-200"
+                    >
+                        Submit
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => navigate('/admin/testimonials')}
+                        className="bg-gray-600 text-white py-2 px-6 rounded-lg font-bold hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 transition duration-200"
+                    >
+                        Back
+                    </button>
+                </div>
+            </form>
+
+        
+        </div>
+    );
+};
+
+export default AddEditTestimonial;
---
+++ cdp-frontend/src/components/admin/manage/Testimonials/AdminTestimonials.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { FaPlus } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Pagination from '../Graduates/Pagination'; // Reusing Pagination Component
+
+const AdminTestimonials = () => {
+    const [testimonials, setTestimonials] = useState([]);
+    const [filteredTestimonials, setFilteredTestimonials] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [searchTerm, setSearchTerm] = useState('');
+    const testimonialsPerPage = 10;
+
+    const fetchTestimonials = async (page = 1) => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/testimonials`, {
+                params: {
+                    page: page,
+                    limit: testimonialsPerPage,
+                    searchTerm: searchTerm,
+                },
+            });
+            setTestimonials(response.data.data);
+            setFilteredTestimonials(response.data.data); // Backend handles the filtering
+            setTotalPages(response.data.totalPages);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchTestimonials(currentPage);
+    }, [currentPage, searchTerm]);
+
+    const handleDelete = async (id) => {
+        const confirmDelete = window.confirm('Are you sure you want to delete this testimonial?');
+        if (!confirmDelete) return;
+
+        try {
+            const token = localStorage.getItem('accessToken');
+            if (!token) return;
+
+            const config = { headers: { Authorization: `Bearer ${token}` } };
+            await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/api/v1/testimonials/${id}`, config);
+
+            // After deletion, refetch the testimonials from the server
+            fetchTestimonials(currentPage);
+
+            alert('Testimonial Deleted Successfully');
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    };
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    return (
+        <div className="container mx-auto p-4">
+            <div className="w-full max-w-5xl mx-auto">
+                <div className="flex flex-col md:flex-row md:justify-between items-center mb-4">
+                    <h2 className="text-2xl font-bold text-blue-900 mb-4 md:mb-0">Testimonials List</h2>
+                    <div className="flex flex-col md:flex-row gap-4 w-full md:w-auto">
+                        <input
+                            type="text"
+                            placeholder="Search by Name or Message"
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                            className="w-full md:w-auto py-2 px-4 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-900"
+                        />
+                        <Link
+                            to="/admin/testimonials/add"
+                            className="w-full md:w-auto inline-flex items-center justify-center bg-blue-900 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300"
+                        >
+                            <FaPlus className="mr-2" /> New
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="overflow-x-auto">
+                    <table className="min-w-full table-auto bg-white rounded-lg shadow-md mb-6">
+                        <thead>
+                            <tr>
+                                <th className="py-2 px-3 text-center bg-blue-100 border-b">#</th>
+                                <th className="py-2 px-3 text-left bg-blue-100 border-b">Name</th>
+                                <th className="py-2 px-3 text-left bg-blue-100 border-b">Message</th>
+                                <th className="py-2 px-3 text-left bg-blue-100 border-b">Title</th>
+                                <th className="py-2 px-3 text-center bg-blue-100 border-b">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredTestimonials.length > 0 ? (
+                                filteredTestimonials.map((testimonial, index) => (
+                                    <tr key={index} className="border-b hover:bg-gray-100 transition duration-300">
+                                        <td className="py-2 px-3 text-center">{index + 1}</td>
+                                        <td className="py-2 px-3">{testimonial.name}</td>
+                                        <td className="py-2 px-3">{testimonial.message}</td>
+                                        <td className="py-2 px-3">{testimonial.title}</td>
+                                        <td className="py-2 px-3 text-center">
+                                            <Link to={`/admin/testimonials/edit/${testimonial._id}`}>
+                                                <button className="bg-yellow-500 text-white py-1 px-2 rounded hover:bg-yellow-600 transition duration-300 mr-2">
+                                                    Edit
+                                                </button>
+                                            </Link>
+                                            <button
+                                                className="bg-red-500 text-white py-1 px-2 rounded hover:bg-red-600 transition duration-300"
+                                                onClick={() => handleDelete(testimonial._id)}
+                                            >
+                                                Delete
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan={5} className="py-4 text-center text-gray-500">No Testimonials Available</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="flex justify-center mt-6">
+                    <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdminTestimonials;
