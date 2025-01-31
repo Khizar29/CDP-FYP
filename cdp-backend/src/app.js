357666ca68b26b75e74ba1c2b910787498b04b3f
Khizar Ali <s.khizarali03@gmail.com>
2025-01-31 20:43:55 +0500

    recruiter routes added

--- cdp-backend/src/app.js
+++ cdp-backend/src/app.js
@@ -31,13 +31,14 @@ import jobRouter from './routes/job.routes.js';
 import graduateRouter from './routes/graduate.routes.js';
 import testimonialRouter from "./routes/testimonial.routes.js";
 import newsfeedRouter from './routes/newsfeed.routes.js';
-
+import recruiterRouter from './routes/recruiter.routes.js';
 // Routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/jobs", jobRouter);
 app.use("/api/v1/graduates", graduateRouter);
 app.use("/api/v1/testimonials",testimonialRouter);
 app.use("/api/v1/newsfeeds", newsfeedRouter);
+app.use("/api/v1/recruiters", recruiterRouter);
 
 // Serve static files from the React frontend app's build folder
 app.use(express.static(path.join(__dirname, '../../cdp-frontend/build')));
