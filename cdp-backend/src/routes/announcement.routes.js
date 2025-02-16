999bc8a825c827a0329b672f633bcccac63267b4
Khizar Ali <s.khizarali03@gmail.com>
2025-02-16 23:22:51 +0500

    fetch by id added

--- cdp-backend/src/routes/announcement.routes.js
+++ cdp-backend/src/routes/announcement.routes.js
@@ -4,19 +4,20 @@ import {
   getAnnouncements,
   updateAnnouncement,
   deleteAnnouncement,
+  getAnnouncementsById
 } from "../controllers/announcement.controller.js";
 import { verifyJWT, verifyRole } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-// Public route
-router.get("/", getAnnouncements);
+router.get("/", verifyJWT, getAnnouncements);
+router.get("/:announcementId", verifyJWT, getAnnouncementsById);
 
 // Protected routes (Admin & Faculty)
 router.post("/", verifyJWT, verifyRole(["admin", "faculty"]), createAnnouncement);
 router.put("/:announcementId", verifyJWT, verifyRole(["admin", "faculty"]), updateAnnouncement);
 
 // Admin-only route
-router.delete("/:announcementId", verifyJWT, verifyRole(["admin"]), deleteAnnouncement);
+router.delete("/:announcementId", verifyJWT, verifyRole(["admin", "faculty"]), deleteAnnouncement);
 
 export default router;
